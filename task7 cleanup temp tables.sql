b761a40652a6a437ef255bfe1f354babb787caa0
Momna <alimomna87@gmail.com>
2026-08-19 11:53:02 -0400

    Delete task7 cleanup temp tables.sql

--- task7 cleanup temp tables.sql
+++
@@ -1,5 +0,0 @@
--- Run this by itself whenever you're re-running Step 1 (or any step)
--- individually instead of the whole script top-to-bottom.
-IF OBJECT_ID('tempdb..#CustomerAttrition') IS NOT NULL DROP TABLE #CustomerAttrition;
-IF OBJECT_ID('tempdb..#SampleCustomers') IS NOT NULL DROP TABLE #SampleCustomers;
-IF OBJECT_ID('tempdb..#BatSmtSample') IS NOT NULL DROP TABLE #BatSmtSample;
